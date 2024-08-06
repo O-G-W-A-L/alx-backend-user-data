d19842a81263f5433df9d68685ce41f9534cc903
O-G-W-A-L <ogwalja04@gmail.com>
2024-08-06 13:04:16 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 from api.v1.auth.auth import Auth
 import re
 from typing import Optional
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -20,4 +22,20 @@ class BasicAuth(Auth):
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
+        return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: Optional[str]
+            ) -> Optional[str]:
+        """Decodes a base64-encoded authorization header
+        """
+        if isinstance(base64_authorization_header, str):
+            try:
+                res = base64.b64decode(
+                        base64_authorization_header,
+                        validate=True)
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
         return None
