ca6670737097e5c50997079d01d3488bc87c7524
O-G-W-A-L <ogwalja04@gmail.com>
2024-08-06 12:39:09 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic Authentication module for the API.
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """Basic Authentication class inheriting from Auth.
+    """
+    pass
