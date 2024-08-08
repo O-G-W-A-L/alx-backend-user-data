473a357de79baad55cf684fa75abafe1932901ca
O-G-W-A-L <ogwalja04@gmail.com>
2024-08-08 09:01:35 +0000

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-"""Basic Authentication module for the API.
+"""Basic authentication module for the API.
 """
-from api.v1.auth.auth import Auth
 import re
-from typing import TypeVar
 import base64
 import binascii
+from typing import Tuple, TypeVar
+
+from .auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
-    """Basic Authentication class inheriting from Auth.
+    """Basic authentication class.
     """
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
-        for Basic Authentication
+        for a Basic Authentication.
         """
-        if isinstance(authorization_header, str):
-            pattern = r'^Basic (?P<token>.+)$'
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
@@ -26,33 +28,34 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
             self,
-            base64_authorization_header: str
+            base64_authorization_header: str,
             ) -> str:
-        """Decodes a base64-encoded authorization header
+        """Decodes a base64-encoded authorization header.
         """
-        if isinstance(base64_authorization_header, str):
+        if type(base64_authorization_header) == str:
             try:
                 res = base64.b64decode(
-                        base64_authorization_header,
-                        validate=True)
+                    base64_authorization_header,
+                    validate=True,
+                )
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
-        return None
 
     def extract_user_credentials(
             self,
-            decoded_base64_authorization_header: str
-            ) -> (str, str):
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
         """Extracts user credentials from a base64-decoded authorization
-        header that uses the Basic authentication flow
+        header that uses the Basic authentication flow.
         """
-        if isinstance(decoded_base64_authorization_header, str):
+        if type(decoded_base64_authorization_header) == str:
             pattern = r'(?P<user>[^:]+):(?P<password>.+)'
             field_match = re.fullmatch(
-                    pattern,
-                    decoded_base64_authorization_header.strip())
-            if field_match:
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
                 user = field_match.group('user')
                 password = field_match.group('password')
                 return user, password
