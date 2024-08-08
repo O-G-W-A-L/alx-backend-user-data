c4de8e6c2569bf7233b1c12b50115ae811074cbe
O-G-W-A-L <ogwalja04@gmail.com>
2024-08-08 10:52:38 +0000

    empty

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Session authentication module for the API.
+"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session authentication class.
+    """
+    pass
